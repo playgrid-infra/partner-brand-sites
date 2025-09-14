e389aaf0f2f550b785cf2b7bb99a1e565edd46b1
Ryan <ryan@pathway-solutions.de>
2025-09-14 21:01:09 +0800

    added based name

--- theupperdeck/vite.config.ts
+++ theupperdeck/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/partner-brand-sites/', 
   server: {
     host: "::",
     port: 8080,
