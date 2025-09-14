d3208bac099144334d152c3a675e86f5498343fb
Ryan <ryan@pathway-solutions.de>
2025-09-14 21:18:01 +0800

    updated based

--- theupperdeck/vite.config.ts
+++ theupperdeck/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/partner-brand-sites/', 
+  base: '/partner-brand-sites', 
   server: {
     host: "::",
     port: 8080,
